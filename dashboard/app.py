6afe8c7da3f2736b11f34de20f34e6ca01d1669f
Pennon <pennonshue@gmail.com>
2024-02-03 12:08:55 -0500

    add visit events table

--- dashboard/app.py
+++ dashboard/app.py
@@ -9,4 +9,20 @@ EVENT_TYPES = [
 ]
 selected_event_type = st.sidebar.selectbox("Select Event Type", EVENT_TYPES)
 
-print(selected_event_type)
+if (selected_event_type == 'Visit Events'):
+    visit_sorted = sorted(visit_events, key=lambda event: event.event_properties.date)
+    # Convert the sorted list to a DataFrame for display
+    data = [{
+        'Page URL': event.event_properties.pageUrl,
+        'User ID': event.event_properties.user_id,
+        'Date': event.event_properties.date
+    } for event in visit_sorted]
+
+    df = pd.DataFrame(data)
+
+    df['Date'] = pd.to_datetime(df['Date'])
+
+    #cap num of rows
+    df = df.head(20)
+    df = df.sort_values(by='Date', ascending=False)
+    st.table(df)
