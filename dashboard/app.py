52c55f0413c92a0dd049e4f780a97ceb8a6cb54d
Samrat Sahoo <samratsahoo2013@gmail.com>
2024-02-04 12:35:59 -0500

    minor bug fixes

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,28 +1,28 @@
+from datetime import datetime
 import streamlit as st
 import pandas as pd
 import numpy as np
 from data import visit_events, click_events, input_events
 
-st.title('Hello World!')
-EVENT_TYPES = [
-    'Visit Events', 'Click Events', 'Input Events'
-]
+st.title("Hello World!")
+EVENT_TYPES = ["Visit Events", "Click Events", "Input Events"]
 selected_event_type = st.sidebar.selectbox("Select Event Type", EVENT_TYPES)
 
-if (selected_event_type == 'Visit Events'):
+if selected_event_type == EVENT_TYPES[0]:
     visit_sorted = sorted(visit_events, key=lambda event: event.event_properties.date)
-    # Convert the sorted list to a DataFrame for display
-    data = [{
-        'Page URL': event.event_properties.pageUrl,
-        'User ID': event.event_properties.user_id,
-        'Date': event.event_properties.date
-    } for event in visit_sorted]
+    data = [
+        {
+            "Page URL": event.event_properties.pageUrl,
+            "User ID": event.event_properties.user_id,
+            "Date": event.event_properties.date,
+        }
+        for event in visit_sorted
+    ]
 
     df = pd.DataFrame(data)
+    df["Date"] = pd.to_datetime(df["Date"])
+    df["Date"] = df["Date"].dt.strftime("%B %d, %Y %I:%M %p")
 
-    df['Date'] = pd.to_datetime(df['Date'])
-
-    #cap num of rows
-    df = df.head(20)
-    df = df.sort_values(by='Date', ascending=False)
+    df = df.sort_values(by="Date", ascending=False)
+    df = df.head(5)
     st.table(df)
