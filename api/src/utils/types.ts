6aff824482db625802aa332a54505b4b01e17ac2
Keerth1Keerthi <KeerthiKonuganti@gmail.com>
2024-02-04 18:16:35 -0500

    feat/add visit and input events to types

--- api/src/utils/types.ts
+++ api/src/utils/types.ts
@@ -8,6 +8,24 @@ export interface BaseEvent extends Document {
     projectId: string | Types.ObjectId;
 }
 
+export interface VisitEvent extends BaseEvent {
+    eventProperties: VisitEventProperties
+}
+
+export interface VisitEventProperties {
+    pageUrl: string;
+    userId: string;
+    date: Date;
+}
+export interface InputEvent extends BaseEvent {
+    eventProperties: InputEventProperties
+}
+
+export interface InputEventProperties {
+    objectId: string;
+    userId: string;
+    textValue: string;
+}
 export interface ClickEvent extends BaseEvent {
     eventProperties: ClickEventProperties
 }
