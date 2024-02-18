4c46081f65e1d7ec9e821b8c3afe32ee3893cd67
Pennon <pennonshue@gmail.com>
2024-02-18 11:11:22 -0500

    update changes for mock api calls

--- dashboard/api.py
+++ dashboard/api.py
@@ -1,7 +1,14 @@
 import requests
+base_url = "https://analytics.bitsofgood.org"
 
-def get_visit_events(project_name, after_time, limit, after_id=None)
-    url = "analytics.bitsofgood.org/api/events/visit-events"
+def get_default_after_time():
+    thirty_days_ago = datetime.now() - timedelta(days=30)
+    return int(time.mktime(thirty_days_ago.timetuple()))
+
+def get_paginated_visit_events(project_name, after_time=None, limit=10, after_id=None)
+    if after_time is None:
+        after_time = get_default_after_time()
+    url = base_url + "/api/events/visit-events"
     params = {
         "projectName": project_name,
         "afterTime": after_time,
@@ -15,9 +22,14 @@ def get_visit_events(project_name, after_time, limit, after_id=None)
         response.raise_for_status()  # error
         return response.json()
     except requests.exceptions.RequestException as err:
-        return {"error": "Request failed", "details": str(err)}
+        return {
+            "success": False,
+            "payload": {
+                "error": str(err)
+            }
+        }
 
-def visit_events()
+def get_visit_events()
     visit_events = []
     while True:
         page = get_visit_events(project_name, after_time, limit, after_id)
@@ -31,8 +43,10 @@ def visit_events()
     return visit_events
 
 
-def get_click_events(project_name, after_time, limit, after_id=None)
-    url = "analytics.bitsofgood.org/api/events/click-events"
+def get_paginated_click_events(project_name, after_time=None, limit=10, after_id=None)
+    if after_time is None:
+        after_time = get_default_after_time()
+    url = base_url + "/api/events/click-events"
     params = {
         "projectName": project_name,
         "afterTime": after_time,
@@ -46,9 +60,14 @@ def get_click_events(project_name, after_time, limit, after_id=None)
         response.raise_for_status()  # error
         return response.json()
     except requests.exceptions.RequestException as err:
-        return {"error": "Request failed", "details": str(err)}
+        return {
+            "success": False,
+            "payload": {
+                "error": str(err)
+            }
+        }
 
-def click_events()
+def get_click_events()
     click_events = []
     while True:
         page = get_click_events(project_name, after_time, limit, after_id)
@@ -61,8 +80,11 @@ def click_events()
             break
     return click_events
 
-def get_input_events(project_name, after_time, limit, after_id=None)
-    url = "analytics.bitsofgood.org/api/events/input-events"
+def get_paginated_input_events(project_name, after_time=None, limit=10, after_id=None)
+    if after_time is None:
+        after_time = get_default_after_time()
+
+    url = base_url + "/api/events/input-events"
     params = {
         "projectName": project_name,
         "afterTime": after_time,
@@ -70,15 +92,19 @@ def get_input_events(project_name, after_time, limit, after_id=None)
     }
     if after_id:
         params["afterId"] = after_id
-
     try:
         response = requests.get(url, params=params)
         response.raise_for_status()  # error
         return response.json()
     except requests.exceptions.RequestException as err:
-        return {"error": "Request failed", "details": str(err)}
+        return {
+            "success": False,
+            "payload": {
+                "error": str(err)
+            }
+        }
 
-def input_events()
+def get_input_events()
     input_events = []
     while True:
         page = get_input_events(project_name, after_time, limit, after_id)
