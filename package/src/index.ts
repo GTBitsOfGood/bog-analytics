122b1f1d504450ba72df8918e0917bd9e072c819
Samrat Sahoo <samratsahoo2013@gmail.com>
2024-03-29 20:06:04 -0400

    update package imports

--- package/src/index.ts
+++ package/src/index.ts
@@ -1,8 +1,31 @@
 import AnalyticsLogger from "@/lib/analytics-logger";
 import AnalyticsManager from "@/lib/analytics-manager";
-
+import AnalyticsViewer from "@/lib/analytics-viewer";
+import {
+    EventEnvironment,
+    ClickEvent,
+    ClickEventProperties,
+    VisitEvent,
+    VisitEventProperties,
+    InputEvent,
+    InputEventProperties,
+    CustomEvent,
+    CustomEventType,
+    CustomGraphType
+} from "@/utils/types";
 
 export {
     AnalyticsLogger,
-    AnalyticsManager
+    AnalyticsManager,
+    AnalyticsViewer,
+    EventEnvironment,
+    VisitEvent,
+    VisitEventProperties,
+    ClickEvent,
+    ClickEventProperties,
+    InputEvent,
+    InputEventProperties,
+    CustomEvent,
+    CustomEventType,
+    CustomGraphType
 };
