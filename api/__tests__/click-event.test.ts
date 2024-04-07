287ecfe2fa895d50187306bfbb095ecff0f0134a
Keerth1Keerthi <KeerthiKonuganti@gmail.com>
2024-04-07 01:02:02 -0400

    feat: pagination click, input, visit tests

--- api/__tests__/click-event.test.ts
+++ api/__tests__/click-event.test.ts
@@ -1,7 +1,7 @@
 import { afterAll, beforeAll, describe, expect, test, afterEach } from '@jest/globals';
 import request, { Test } from "supertest";
 import { api } from "@/netlify/functions/api";
-import { Project } from '@/src/utils/types';
+import { ClickEvent, Project } from '@/src/utils/types';
 import { deleteProjectById } from '@/src/actions/project';
 import { Server, IncomingMessage, ServerResponse } from 'http';
 import TestAgent from 'supertest/lib/agent';
@@ -137,4 +137,167 @@ describe("/api/events/click-event", () => {
 
         });
     });
-});
+    describe("GET /api/events/click-event", () => {
+        afterEach(async () => {
+            await deleteClickEvents();
+        });
+
+
+        test("Get pagination with zero events", async () => {
+            const response = await agent
+                .get("/api/events/click-event")
+                .query({ projectName: testProject?.projectName })
+            expect(response.status).toBe(200);
+            const events = response.body.payload.events;
+            const afterId = response.body.payload.afterId;
+            expect(events.length).toEqual(0);
+            expect(afterId).toBeNull();
+
+        });
+        test("Get pagination with one event", async () => {
+            let clickEventProperties = {
+                objectId: "id",
+                userId: "userId",
+                environment: "development"
+            }
+            const response = await agent
+                .post("/api/events/click-event")
+                .set("clienttoken", testProject?.clientApiKey as string)
+                .send(clickEventProperties);
+            expect(response.status).toBe(200);
+            const event = response.body.payload;
+
+
+            const getResponse = await agent
+                .get("/api/events/click-event")
+                .query({ projectName: testProject?.projectName })
+            expect(getResponse.status).toBe(200);
+            const events = getResponse.body.payload.events;
+            const afterId = getResponse.body.payload.afterId;
+            expect(events.length).toEqual(1);
+            expect(afterId).toEqual(event._id);
+            expect(events[0]).toEqual(event);
+        });
+
+        test("Get pagination with one event", async () => {
+            let clickEventProperties = {
+                objectId: "id",
+                userId: "userId",
+                environment: "development"
+            }
+            const response = await agent
+                .post("/api/events/click-event")
+                .set("clienttoken", testProject?.clientApiKey as string)
+                .send(clickEventProperties);
+            expect(response.status).toBe(200);
+            const event = response.body.payload;
+
+
+            const getResponse = await agent
+                .get("/api/events/click-event")
+                .query({ projectName: testProject?.projectName })
+            expect(getResponse.status).toBe(200);
+            const events = getResponse.body.payload.events;
+            const afterId = getResponse.body.payload.afterId;
+            expect(events.length).toEqual(1);
+            expect(afterId).toEqual(event._id);
+            expect(events[0]).toEqual(event);
+        });
+        test("Normal pagination with 15 event and 5 limit", async () => {
+            //populate 15 events
+            let batches: ClickEvent[] = [];
+            for (let i = 0; i < 15; i++) {
+                let clickEventProperties = {
+                    objectId: "id" + i,
+                    userId: "userId",
+                    environment: "development"
+                }
+                const response = await agent
+                    .post("/api/events/click-event")
+                    .set("clienttoken", testProject?.clientApiKey as string)
+                    .send(clickEventProperties);
+                expect(response.status).toBe(200);
+                const event = response.body.payload;
+                batches.push(event);
+            }
+
+            let afterId = null;
+            for (let i = 0; i < 3; i++) {
+                const getResponse = await agent
+                    .get("/api/events/click-event")
+                    .query({ projectName: testProject?.projectName, limit: 5, afterId })
+                expect(getResponse.status).toBe(200);
+                const events = getResponse.body.payload.events;
+                afterId = getResponse.body.payload.afterId;
+                expect(events.length).toEqual(5);
+                expect(afterId).toEqual(batches[i * 5 + 4]._id);
+                for (let j = 0; j < 5; j++) {
+                    expect(events[j]).toEqual(batches[i * 5 + j]);
+                }
+            }
+        });
+        test("Pagination with 15 event and 4 limit", async () => {
+            //populate 15 events
+            let batches: ClickEvent[] = [];
+            for (let i = 0; i < 15; i++) {
+                let clickEventProperties = {
+                    objectId: "id" + i,
+                    userId: "userId",
+                    environment: "development"
+                }
+                const response = await agent
+                    .post("/api/events/click-event")
+                    .set("clienttoken", testProject?.clientApiKey as string)
+                    .send(clickEventProperties);
+                expect(response.status).toBe(200);
+                const event = response.body.payload;
+                batches.push(event);
+            }
+
+            let afterId = null;
+            for (let i = 0; i < 3; i++) {
+                const getResponse = await agent
+                    .get("/api/events/click-event")
+                    .query({ projectName: testProject?.projectName, limit: 4, afterId })
+                expect(getResponse.status).toBe(200);
+                const events = getResponse.body.payload.events;
+                afterId = getResponse.body.payload.afterId;
+                let length = Math.min(4, 15 - i * 4)
+                expect(events.length).toEqual(length);
+                expect(afterId).toEqual(batches[i * 4 + length - 1]._id);
+                for (let j = 0; j < length; j++) {
+                    expect(events[j]).toEqual(batches[i * 4 + j]);
+                }
+            }
+        });
+        test("Pagination with after time greater than all times", async () => {
+            //populate 15 events
+            let batches: ClickEvent[] = [];
+            for (let i = 0; i < 15; i++) {
+                let clickEventProperties = {
+                    objectId: "id" + i,
+                    userId: "userId",
+                    environment: "development"
+                }
+                const response = await agent
+                    .post("/api/events/click-event")
+                    .set("clienttoken", testProject?.clientApiKey as string)
+                    .send(clickEventProperties);
+                expect(response.status).toBe(200);
+                const event = response.body.payload;
+                batches.push(event);
+            }
+
+            let afterId = null;
+            const getResponse = await agent
+                .get("/api/events/click-event")
+                .query({ projectName: testProject?.projectName, afterId, afterTime: new Date(Date.now() + 1000000) })
+            expect(getResponse.status).toBe(200);
+            const events = getResponse.body.payload.events;
+            afterId = getResponse.body.payload.afterId;
+            expect(events.length).toEqual(0);
+            expect(afterId).toBeNull();
+        });
+    });
+})
+
